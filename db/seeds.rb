42965f7ba4df17e87b0e9d6efa42307901e8048e
John Leosco <jrleosco@gmail.com>
2021-11-15 15:15:40 -0700

    database seeded

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,13 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+require "faker"
+
+10.times do
+  Note.create(
+    title: Faker::NatoPhoneticAlphabet.code_word,
+    note: Faker::Quote.famous_last_words,
+  )
+end
